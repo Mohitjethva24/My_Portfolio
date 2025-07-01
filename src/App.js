e7df7ede7b711b3f4381faa89dcd93a8fe0c564b
Mohit jethva <mohitjethva24@gmail.com>
2025-07-01 22:48:06 +0530

    fix: removed unused variables to fix CI build error

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import About from "./Pages/About";
 import Contact from "./Pages/Contact";
 import Projects from "./Pages/Projects";
 import Technologies from "./Pages/Technologies";
-import { Link } from "react-router-dom";
+
 function App() {
   return (
     <Router>
